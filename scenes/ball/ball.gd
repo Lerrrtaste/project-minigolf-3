e0ed310e6b4e5f2bec7d5a10588d467fb3009540
Lerrrtaste <lerrrtaste@protonmail.com>
2021-09-23 21:06:15 +0200

    Started switching to KinematicBody2D for the Ball

--- scenes/ball/ball.gd
+++ scenes/ball/ball.gd
@@ -1,22 +1,23 @@
-extends RigidBody2D
+extends KinematicBody2D
 
+var velocity := Vector2()
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
 	update()
+	
+	var collision = move_and_collide(velocity)
+	print(collision)
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
-			apply_central_impulse(get_local_mouse_position().rotated(rotation))
+			if velocity == Vector2():
+				velocity = get_local_mouse_position()
+			#apply_central_impulse(get_local_mouse_position().rotated(rotation))
 	
 func _draw():
-	draw_line(Vector2(), get_local_mouse_position(), ColorN("red"), 3.0, true)
+	if velocity == Vector2():
+		draw_line(Vector2(), get_local_mouse_position(), ColorN("red"), 3.0, true)
