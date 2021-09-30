a1d5e6329bae16aa41c630a83abb57e373f4a190
Lerrrtaste <lerrrtaste@protonmail.com>
2021-09-30 14:54:55 +0200

    Started changing to Node based Course instead of Tileset

--- scenes/course/course.gd
+++ scenes/course/course.gd
@@ -1,12 +1,45 @@
-extends TileMap
+extends Node2D
 
-var title:String = "Not Loaded"
-var id:String = "a8sjk2" #unique and collection key
-var loaded := false
+enum States {
+	INVALID = -1, # not loaded
+	PLAYING,
+	EDITING,
+}
+var state:int = States.INVALID
+
+var metadata := { #keys here are required when loading, values here are placeholders
+	"title": "Not Loaded",
+	"id": "#000000",#unique and collection key
+	"version": "0.0.0.0"
+}
 
 func _ready():
-	pass # Replace with function body.
+	set_process(false)
 
+func setup(new_course:Dictionary):
+	_load_from_dictionary(new_course)
+
+func _load_from_dictionary(course:Dictionary)->bool:
+	if !course.has("metadata"):
+		return false
+	
+	if course.has("map"):
+		return false
+	
+	# metadata
+	var metadata_new = course["metadata"]
+	for key in metadata.keys(): # validate all required keys
+		if !metadata_new.has(key):
+			return false
+	metadata = metadata_new
+	
+	#assert()
+	
+	#map
+	
+	return true
+
+func _spawn_map(map:Dictionary)
 # TODOS
 # load
 # save
