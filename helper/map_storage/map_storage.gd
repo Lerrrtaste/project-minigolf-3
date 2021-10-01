288cda13e335c1c1716686158ef054f070d4588c
Lerrrtaste <lerrrtaste@protonmail.com>
2021-10-01 21:27:55 +0200

    Continued work on MapStorage saving and loading; Metadata save/load is wokring

--- helper/map_storage/map_storage.gd
+++ helper/map_storage/map_storage.gd
@@ -1,14 +1,15 @@
 extends Node
 
-
 var loaded_maps := {}
 
 func _ready():
 	pass#_load_bultin_maps()
 
 
-func get_map(map_id:String) -> Dictionary:
-	var packed_map := _load_builtin_map(map_id) # look builtin
+func get_packed_map(map_id:String) -> Dictionary:
+	var packed_map:Dictionary
+	
+	packed_map = _load_builtin_map(map_id) # look if map is builtin first (maybe special id later)
 	if !packed_map.empty():
 		return packed_map
 	
@@ -17,6 +18,28 @@ func get_map(map_id:String) -> Dictionary:
 	return {}
 
 
+#lists all JSON Filenames in MAPFILES_BUILTIN_PATH
+func list_builtin_map_ids() -> Array:
+	var ids:Array
+	
+	var dir := Directory.new()
+	if dir.open(Global.MAPFILES_PATH_BUILTIN) != OK:
+		emit_signal("error","Error","Could not open builtin map dir")
+		return []
+	
+	#append all file names without extension
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	while file_name != "":
+		if !dir.current_is_dir() && file_name.ends_with(".json"):
+			ids.append(file_name.substr(0,file_name.length()-5)) # remove .json
+		file_name = dir.get_next()
+	dir.list_dir_end()
+	
+	return ids
+
+
+# Loads specific mapfile from MAPFILES_BUILTIN_PATH
 func _load_builtin_map(map_id:String)->Dictionary:
 	var dir := Directory.new()
 	if dir.open(Global.MAPFILES_PATH_BUILTIN) != OK:
@@ -30,9 +53,10 @@ func _load_builtin_map(map_id:String)->Dictionary:
 	var file := File.new()
 	file.open(Global.MAPFILES_PATH_BUILTIN+map_id+".json", File.READ)
 	var packed_map_jstring := file.get_as_text()
-	var check = JSON.validate_json(packed_map_jstring)
-	if check != "":
-		emit_signal("error","Error","Loaded mapfile is not a valid JSON object: %s"%check)
+	file.close()
+	var parseresult = JSON.parse(packed_map_jstring)
+	if parseresult.error != OK:
+		emit_signal("error","Error","Loaded mapfile is not a valid JSON object: %s"%parseresult.error_string)
 		return {}
 	
-	return JSON.parse_json(packed_map_jstring).result
+	return parseresult.result
--- scenes/map/map.gd
+++ scenes/map/map.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+class_name Map
+
 enum States {
 	INVALID = -1, # not loaded
 	LOADED,
@@ -13,7 +15,7 @@ var mapdata:Dictionary #contains blocks (key=vec2, val=block_id)
 var metadata := {
 	"title": "Not Loaded",
 	"id": "#000000",#unique and collection key
-	"version": "0.0.0.0"
+	"version": "0.0.0.0",
 }#keys here are required when loading, values here are placeholders
 
 
@@ -29,41 +31,63 @@ func _ready():
 	set_process(false)
 
 
-func load_from_coursedata(packed_map:Dictionary):
+func load_packed_map(packed_map:Dictionary):
+	assert(_state == States.INVALID) #not yet loaded
 	_load_from_dictionary(packed_map)
 	_spawn_map()
 	_change_state(States.LOADED)
 
+
+func create_new(title:String,id:String)->void:
+	assert(_state == States.INVALID) #not yet loaded
+	metadata = {
+		"title": title,
+		"id": id,
+		"version": Global.VERSION
+	}
+	_change_state(States.LOADED)
+
+
 func set_mode_editing()->void:
 	_change_state(States.EDITING)
+
 
 func set_mode_playing()->void:
 	_change_state(States.PLAYING)
 
+
 func _change_state(new_state:int)->void:
 	match new_state:
 		States.LOADED:
-			emit_signal("map_loaded",metadata.id)
+			Signals.emit_signal("map_loaded",metadata.id)
 			print("Map loaded")
 		
 		_:
-			if _state != States.INVALID:
-				emit_signal("error","Error","Map has not been loaded")
+			if _state == States.INVALID:
+				Signals.emit_signal("error","Error","Map has not been loaded")
 				assert(false)
 			
 			continue
 		
 		States.PLAYING:
-			emit_signal("map_started_playing",metadata.id)
+			Signals.emit_signal("map_started_playing",metadata.id)
 			print("Map playing")
 			
 		States.EDITING:
-			emit_signal("map_started_editing",metadata.id)
+			Signals.emit_signal("map_started_editing",metadata.id)
 			print("Map editing")
 			
 		States.COMPLETED:
-			emit_signal("map_started_completed",metadata.id)
+			Signals.emit_signal("map_started_completed",metadata.id)
 	_state = new_state
+
+
+func get_as_packed_map()->Dictionary:
+	var ret = {
+			"mapdata": mapdata,
+			"metadata": metadata,
+			}
+	return ret
 
 
 # Converts any Vector2 coordinates or motion from the cartesian to the isometric system
@@ -82,21 +106,21 @@ func isometric_to_cartesian(iso):
 
 func _load_from_dictionary(packed_map:Dictionary)->bool:
 	if !packed_map.has("metadata"):
-		emit_signal("error","Error","Trying to load mapdata")
+		Signals.emit_signal("error","Error","Trying to load mapdata")
 		return false
 	
 	# metadata
-	var metadata_new = mapdata["metadata"]
+	var metadata_new = packed_map["metadata"]
 	for key in metadata.keys(): # validate all required keys
 		if !metadata_new.has(key):
-			return false
+			Signals.emit_signal("error","Error","PackedMap missing required key %s"%key)
 	metadata = metadata_new
 	mapdata.erase("metadata")
 	
 	#map
 	for key in packed_map.keys():
 		if !block_classes.has(packed_map[key]):
-			emit_signal("error","Error","Mapdata contains unknown blocks")
+			Signals.emit_signal("error","Error","Mapdata contains unknown blocks")
 			return false
 	
 	mapdata = packed_map
@@ -110,12 +134,7 @@ func _spawn_map()->void:
 		blocks.add_child(inst)
 
 
-func _map_to_dictionary()->Dictionary:
-	var ret = {
-			"mapdata": mapdata,
-			"metadata": metadata,
-			}
-	return ret
+
 
 
 # TODOS
