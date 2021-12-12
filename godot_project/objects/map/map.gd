2d5581a1beb0e48cfd0b170ae214e14f59e80be1
Lerrrtaste <lerrrtaste@protonmail.com>
2021-12-12 19:57:47 +0100

    Editor Tools working; Added Start and Finish map Objects with sprites; Added Cursor in editor for both;

--- godot_project/objects/map/map.gd
+++ godot_project/objects/map/map.gd
@@ -4,6 +4,7 @@ extends Node2D
 Tile Size
 32*44
 """
+
 const TILE_X = 32
 const TILE_Y = 16
 
@@ -19,6 +20,11 @@ const TILE_DATA = { #id has to be the index in the tilemap
 }
 onready var tilemap = get_node("TileMap")
 
+const OBJECT_DATA = {
+	"start": {"id":0, "scene_path":"res://objects/map_objects/finish/Finish.tscn", "texture_path":"res://objects/map_objects/finish/finish.png"},
+	"finish": {"id":1, "scene_path":"res://objects/map_objects/start/Start.tscn", "texture_path":"res://objects/map_objects/start/start.png"},
+}
+var spawned_objects:Dictionary
 
 func _ready():
 	pass
@@ -27,35 +33,67 @@ func _ready():
 func _process(delta):
 	pass
 
-
-func editor_add_object(world_pos:Vector2,object_id:int):
-	pass
-
-
-func editor_tile_remove(world_pos:Vector2):
-	pass
-
-
-func editor_cursor_show(world_pos:Vector2):
-	pass
-
-
-func editor_cursor_hide(world_pos:Vector2):
-	pass
-
-
 func _unhandled_input(event):
 	if event is InputEventMouse:
 		pass
 
 
+func spawn_map()->void:
+	pass
+
+
+#### Editor Actions
+
+func editor_object_place(world_pos:Vector2,object_id:int):
+	var cell = tilemap.world_to_map(world_pos)
+	
+	if spawned_objects.keys().has(cell):
+		return
+	
+	var obj
+	for i in OBJECT_DATA:
+		if OBJECT_DATA[i]["id"] == object_id:
+			obj = load(OBJECT_DATA[i]["scene_path"]).instance()
+	var snapped_pos = tilemap.map_to_world(cell)
+	snapped_pos.y -= TILE_Y/2 # center on cell
+	obj.position = snapped_pos
+	add_child(obj)
+	spawned_objects[cell] = obj
+
+
+func editor_object_remove(world_pos:Vector2):
+	var cell = tilemap.world_to_map(world_pos)
+	
+	if not spawned_objects.keys().has(cell):
+		return
+		
+	var obj = spawned_objects[cell]
+	obj.visible = false
+	obj.set_process(false)
+	obj.queue_free()
+	spawned_objects.erase(cell)
+
+
+func editor_tile_change(world_pos:Vector2, tile_id:int):
+	var cell = tilemap.world_to_map(world_pos)
+	tilemap.set_cell(cell.x,cell.y,tile_id)
+
+
 #### Loading / Saving
 
+func serialize()->String:
+	return ""
 
-
-
+func deserialize()->String:
+	return ""
 
 #### Helper Functions
+
+func get_center_cell_position(world_pos:Vector2)->Vector2:
+	var cell = tilemap.world_to_map(world_pos)
+	var snapped_pos = tilemap.map_to_world(cell)
+	snapped_pos.y -= TILE_Y/2 # center on cell
+	return snapped_pos
 
 func snap_world_to_grid(pos:Vector2)->Vector2:
 	var grid = Vector2()
@@ -63,12 +101,14 @@ func snap_world_to_grid(pos:Vector2)->Vector2:
 	grid.y = floor(pos.y/TILE_Y) * TILE_Y
 	return grid
 
+
 func cartesian_to_isometric(cart:Vector2)->Vector2:
 	# Cartesian to isometric:
 	var iso = Vector2()
 	iso.x = cart.x - cart.y
 	iso.y = (cart.x + cart.y) / 2
 	return iso
+
 
 func isometric_to_cartesion(iso:Vector2)->Vector2:
 	# Isometric to Cartesian:
--- godot_project/scenes/editor/editor.gd
+++ godot_project/scenes/editor/editor.gd
@@ -1,14 +1,28 @@
 extends Node2D
 
-onready var spr_grid_cursor = get_node("SprGridCursor")
 onready var map = get_node("Map")
-onready var select_tile = get_node("SelectTile")
-onready var tilemap_cursor = get_node("TileMapCursor")
-
+onready var select_tile = get_node("CanvasLayer/UI/SelectTile")
+onready var select_tool = get_node("CanvasLayer/UI/SelectTool")
+onready var select_object = get_node("CanvasLayer/UI/SelectObject")
+onready var tilemap_cursor = get_node("Map/TileMapCursor")
+onready var camera_editor = get_node("CameraEditor")
 var show_cursor := true
+onready var spr_object_cursor = get_node("SprObjectCursor")
 var selected_cell := Vector2()
 
+enum Tools {
+	tile_change,
+	object_place,
+	object_remove,
+}
+
+
+
 func _ready():
+	# center camera
+	camera_editor.position = OS.window_size/2
+	
+	# populate tile dropdown
 	for i in map.TILE_DATA:
 		var tile_id = map.TILE_DATA[i]["id"] + 1
 		if map.TILE_DATA[i]["texture_path"] != null:
@@ -16,11 +30,24 @@ func _ready():
 			select_tile.add_icon_item(icon,i,tile_id)
 		else:
 			select_tile.add_item(i,tile_id)
+	
+	# populate object dropdown
+	for i in map.OBJECT_DATA:
+		var object_id = map.OBJECT_DATA[i].id
+		if map.OBJECT_DATA[i]["texture_path"] != null:
+			var icon = load(map.OBJECT_DATA[i]["texture_path"])
+			select_object.add_icon_item(icon,i,object_id)
+		else:
+			select_object.add_item(i,object_id)
+	
+	#populate tool dropdown
+	select_tool.add_item("Place Tile",Tools.tile_change)
+	select_tool.add_item("Place Object",Tools.object_place)
+	select_tool.add_item("Remove Object",Tools.object_remove)
+	_on_SelectTool_item_selected(0)
 
 
 func _process(delta):
-	var snapped = map.snap_world_to_grid(get_local_mouse_position())
-	spr_grid_cursor.position = snapped
 	
 	var cam_movement := Vector2()
 	var cam_speed = 10
@@ -32,15 +59,29 @@ func _process(delta):
 		cam_movement.x -= cam_speed 
 	if Input.is_key_pressed(KEY_D):
 		cam_movement.x += cam_speed
-	$DebugCamera.position += cam_movement
+	camera_editor.position += cam_movement
 	update()
 	
-	if show_cursor:
-		var coord = tilemap_cursor.world_to_map(get_global_mouse_position())
-		if coord != selected_cell:
-			tilemap_cursor.set_cell(selected_cell.x,selected_cell.y,-1)
-			tilemap_cursor.set_cell(coord.x,coord.y,select_tile.selected-1)
-			selected_cell = coord
+
+
+func _unhandled_input(event):
+	if event is InputEventMouse:
+		
+		# move cursor
+		if show_cursor:
+			var coord = tilemap_cursor.world_to_map(get_global_mouse_position())
+			if coord != selected_cell:
+				match select_tool.selected:
+					Tools.object_place:
+						spr_object_cursor.position = map.get_center_cell_position(get_global_mouse_position())
+					Tools.tile_change:
+						tilemap_cursor.set_cell(selected_cell.x,selected_cell.y,-1)
+						tilemap_cursor.set_cell(coord.x,coord.y,select_tile.selected-1)
+						selected_cell = coord
+		
+		if event.is_pressed():
+			if event.button_mask == BUTTON_LEFT:
+				use_tool()
 
 
 func _draw():
@@ -60,3 +101,30 @@ func _draw():
 #	var mouse = get_local_mouse_position()
 #	var worldpos:Vector2 = (mouse.x / map.TILE_X/2 + mouse.y / map.TILE_Y/2) /2
 #	draw_circle(worldpos,4,ColorN("red"))
+
+
+func use_tool()->void:
+	match select_tool.selected:
+		Tools.tile_change:
+			map.editor_tile_change(get_global_mouse_position(),select_tile.selected-1)
+		Tools.object_place:
+			map.editor_object_place(get_global_mouse_position(),select_object.selected)
+
+
+func _on_SelectTool_item_selected(index):
+	match select_tool.selected:
+		Tools.tile_change:
+			select_tile.visible = true
+			select_object.visible = false
+			spr_object_cursor.visible = false
+			tilemap_cursor.visible = true
+		Tools.object_place:
+			select_tile.visible = false
+			select_object.visible = true
+			tilemap_cursor.visible = false
+			spr_object_cursor.visible = true
+		Tools.object_remove:
+			select_tile.visible = false
+			select_object.visible = false
+			tilemap_cursor.visible = false
+			spr_object_cursor.visible = true
